f1eb250a591a2dd5e42431e891610c63cf80414f
Janek Brandt <brandt.janek@gmail.com>
2017-04-28 11:07:10 -0700

    add question model and question-tile component to display question info

--- app/models/user.js
+++ app/models/user.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr(),
   userID: DS.attr(),
-  photoURL: DS.attr()
+  photoURL: DS.attr(),
+  questions: DS.hasMany('question', {async: true})
 });
