1ac1037bd545138c751f17d1a50f3fb4bda33f83
Janek Brandt <brandt.janek@gmail.com>
2017-04-28 11:49:31 -0700

    add routes and components to add a new question

---
+++ app/components/question-new.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  getUser: Ember.inject.service('get-user'),
+  actions: {
+    saveQuestion() {
+      var userID = this.get('session').get('currentUser').uid;
+      this.get('getUser').getUser(userID).then(user => {
+        var params = {
+          title: this.get('title'),
+          body: this.get('body'),
+          timestamp: (new Date()).toJSON(),
+          user: user
+        };
+        this.sendAction('saveQuestion', params);
+      })
+    }
+  }
+});
