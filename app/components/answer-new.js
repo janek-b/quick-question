93d840f4418134f07c74e103344ba9579baa06d6
Janek Brandt <brandt.janek@gmail.com>
2017-04-29 12:51:03 -0700

    add component to add new answer to a question

---
+++ app/components/answer-new.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  getUser: Ember.inject.service('get-user'),
+  actions: {
+    saveAnswer() {
+      var userID = this.get('session').get('currentUser').uid;
+      this.get('getUser').getUser(userID).then(user => {
+        var params = {
+          body: this.get('body'),
+          timestamp: (new Date()).toJSON(),
+          question: this.get('question'),
+          user: user
+        };
+        this.set('body', '');
+        this.sendAction('saveAnswer', params);
+      })
+    }
+  }
+});
