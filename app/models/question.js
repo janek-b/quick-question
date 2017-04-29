ee46104161ed7b794bb4b37c1d27cc750b52d1e0
Janek Brandt <brandt.janek@gmail.com>
2017-04-29 12:20:32 -0700

    add answer model

--- app/models/question.js
+++ app/models/question.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   title: DS.attr(),
   body: DS.attr(),
   timestamp: DS.attr(),
-  user: DS.belongsTo('user', {async: true})
+  user: DS.belongsTo('user', {async: true}),
+  answers: DS.hasMany('answer', {async: true})
 });
--- app/models/user.js
+++ app/models/user.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   name: DS.attr(),
   userID: DS.attr(),
   photoURL: DS.attr(),
-  questions: DS.hasMany('question', {async: true})
+  questions: DS.hasMany('question', {async: true}),
+  answers: DS.hasMany('answer', {async: true})
 });
